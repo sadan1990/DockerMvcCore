5fb2c17d04b424c08158bf8d4919f101b2c6fe5d
sadan kumar <sadanroy@gmail.com>
2022-05-23 16:34:00 +0530

    Add project files.

---
+++ DockerMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerMvcCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
